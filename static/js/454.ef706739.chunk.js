ff26bce469a7348e374ee28e7437d7372ccd8ecb
Kl0filinj <101584452+Kl0filinj@users.noreply.github.com>
2023-02-03 23:52:08 +0000

    Deploying to gh-pages from @ Kl0filinj/goit-project-petly@bdb2f170a2d7f0333d1bf77b6b193ec571298218 🚀

--- static/js/454.ef706739.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_project_petly=self.webpackChunkgoit_project_petly||[]).push([[454],{4454:function(e,r,n){n.r(r),n.d(r,{OurFriends:function(){return u}});n(2791);var t=n(184),u=function(){return(0,t.jsx)("div",{children:"OurFriends"})}}}]);
-//# sourceMappingURL=454.ef706739.chunk.js.map
